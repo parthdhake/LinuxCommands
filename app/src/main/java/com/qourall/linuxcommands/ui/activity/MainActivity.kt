a32f2466734ff588e1e3b19d2589932375c91b72
parthdhake <parthdhake@gmail.com>
2021-09-02 22:28:11 +0530

    Initial commit

---
+++ app/src/main/java/com/qourall/linuxcommands/ui/activity/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.qourall.linuxcommands.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.qourall.linuxcommands.R
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+    }
+}
---
+++ app/src/main/java/com/qourall/linuxcommands/ui/activity/SplashActivity.kt
@@ -0,0 +1,35 @@
+package com.qourall.linuxcommands.ui.activity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import com.qourall.linuxcommands.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        // This is used to hide the status bar and make
+        // the splash screen as a full screen activity.
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        // we used the postDelayed(Runnable, time) method
+        // to send a message with a delayed time.
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 1000) // 3000 is the delayed time in milliseconds.
+
+    }
+}
